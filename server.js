ea63828afc296887ad5ab2c95c2c0ae3ac43234c
sarahrose9 <78404100+sarahrose9@users.noreply.github.com>
2021-07-29 13:21:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const route=require("./routes/TaskRoute")
+const express = require("express");
+const mongoose = require("mongoose");
+const dbcon=require("./dao/dbcon");
+const route=require("./routes/TaskRoute");
 const app = express()
 app.use(express.json())
-const db = "mongodb://localhost:27017/taskManager"
-mongoose.connect(db,({useNewUrlParser:true}))
-  .then(console.log("Connected to MongoDB"))
-  .catch(err=>console.log(err))
 
-app.use("/TaskRoute",route)
+dbcon();
+app.use("",route)
 app.listen(5000, ()=>{
     console.log("server is running at port 5000")
-
 })

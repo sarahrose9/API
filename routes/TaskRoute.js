46a5729292514014e5024dfa929fec5438edee5f
sarahrose9 <78404100+sarahrose9@users.noreply.github.com>
2021-07-29 13:11:58 +0530

    Update TaskRoute.js

--- routes/TaskRoute.js
+++ routes/TaskRoute.js
@@ -1,11 +1,11 @@
 const express=require('express')
-const TaskRoute= require('../dao/TaskDao')
+const TaskRoute= require('../Controllers/taskController')
 const app = express()
 app.use(express.json())
 
-app.get("/tasks",TaskRoute.find)
+app.get("/tasks",TaskRoute.findAll)
 app.post("/tasks",TaskRoute.add)
-app.delete("/tasks/:id",TaskRoute.delete)
+app.delete("/tasks/:id",TaskRoute.deleteTask)
 app.delete("/tasks",TaskRoute.deleteall)
 
 module.exports=app;
